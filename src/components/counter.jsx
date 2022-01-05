d23df27f9aa51c7f27057664aa7f5820b70dbefb
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-05 12:06:54 +0200

    Updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends React.Component {
   state = {
     //the data the components need
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -25,6 +25,12 @@ class Counter extends React.Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {/* <ul>
           {this.state.tags.map((tag) => (
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,14 +11,19 @@ class Counters extends React.Component {
     ],
   };
 
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters }); //overriding the state
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter} //everything we know about counter is in
           ></Counter>
         ))}
       </div>
